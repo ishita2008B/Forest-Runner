e29cb4b4ff0d64339456eea7ee965265729af74d
ishita2008B <89391021+ishita2008B@users.noreply.github.com>
2021-09-16 17:33:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,9 +6,9 @@ var path,pathImg,forestSound,wolfHowl;
 var fire, fireImg;
 function preload(){
   pathImg = loadImage("depositphotos_211414398-stock-video-halloween-shortcut-road-forest-scary.jpg");
-  playerImg = loadImage("—Pngtree—school girl skateboarding original hand_5749612.png");
+  playerImg = loadAnimation("boy running 1.png","boy running 2.png","boy running 3.png","boy running 4.png");
   forestSound = loadSound("dark-forest.mp3");
-  wolfHowl = loadSound("mixkit-lone-wolf-howling-1729.wav");
+ // wolfHowl = loadSound("mixkit-lone-wolf-howling-1729.wav");
   fireImg = loadImage("fire.png");
   
   
@@ -19,11 +19,11 @@ function setup(){
    path = createSprite(200,300,100,50)
    path.addImage(pathImg);
    path.scale = 4.5;
-   path.velocityX  = 12;
+   path.velocityX  = -30;
     
    player = createSprite(200,300,100,50);
-   player.addImage(playerImg);
-   player.scale = 0.2;
+   player.addAnimation("player_running",playerImg);
+   player.scale = 1;
    player.x = 100
 
   
@@ -41,8 +41,8 @@ function setup(){
    
 
 } function draw(){
-  if (path.x > width){
-    path.x = width/2;
+  if (path.x > -width){
+    path.x = -width/-2;
   }
   
   if(frameCount === 100){
@@ -63,7 +63,7 @@ function setup(){
    
 
    function spawnFire(){
-     fire  = createSprite(random(50,350),40,10,10)
+     fire  = createSprite(Math.round(random,700,900))
      fire.addImage(fireImg);
      fire.scale = 0.5;
 
